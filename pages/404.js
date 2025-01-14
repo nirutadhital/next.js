16873267baa5ead0e18c0ffa8cdd0fcf19deda56
nirutadhital <dhitalniruta99@gmail.com>
2025-01-14 18:47:33 +0545

    feat: add automatic redirection using useEffect and useRouter

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,19 @@
 import Link from "next/link";
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+    const router=useRouter();//method used to redirect the user
+    useEffect(()=>{
+        //console.log('use effect run')
+        setTimeout(()=>{
+            // router.go(-1)//-1---backward rote  +1--forward route
+            router.push('/');
+        },3000)
+
+    },[])
+
+
     return ( 
         <div className="not-found">
             <h1>Ooops.....</h1>

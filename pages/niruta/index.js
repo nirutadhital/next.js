d2310d9506cba9cbe00b9584f55e801d4dee788d
nirutadhital <dhitalniruta99@gmail.com>
2025-01-15 13:30:47 +0545

    feat: implement data fetching from API endpoint

--- pages/niruta/index.js
+++ pages/niruta/index.js
@@ -1,7 +1,18 @@
 import styles from '../../styles/Home.module.css'
+import nirutastyles from '../../styles/niruta.module.css'
 import Head from 'next/head';
 
-const Contact =()=>{
+export const getStaticProps = async() => {
+    const res=await fetch('https://jsonplaceholder.typicode.com/users');
+    const data=await res.json();
+
+    return {
+        props: {users:data}
+    }
+
+}
+
+const Contact =({users})=>{
     return (
         <>
             <Head>  
@@ -11,6 +22,13 @@ const Contact =()=>{
             <div>
                 <h1>Contact Page</h1>
                 <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+                {users.map(user => (   
+                    <div key={user.id}>
+                        <a className={nirutastyles.single}>
+                            <h3>{user.name}</h3>
+                        </a>
+                    </div>   
+                ))}
             </div>
         </>
     );

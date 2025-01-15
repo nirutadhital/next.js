9781089061b16e5a67f7febe2d33b610c9eecef1
nirutadhital <dhitalniruta99@gmail.com>
2025-01-15 12:24:04 +0545

    feat: add image and metadata

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
+                <Image src="/8_1sasa11.jpg" width={128} height={77} alt=""></Image>
             </div>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
--- pages/niruta/index.js
+++ pages/niruta/index.js
@@ -1,11 +1,18 @@
 import styles from '../../styles/Home.module.css'
+import Head from 'next/head';
 
 const Contact =()=>{
     return (
-        <div>
-            <h1>Contact Page</h1>
-            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-        </div>
+        <>
+            <Head>  
+                <title>Niruta | Contact </title>
+                <meta name='keywords' content='niruta'></meta>
+            </Head>
+            <div>
+                <h1>Contact Page</h1>
+                <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            </div>
+        </>
     );
 }
 

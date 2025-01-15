0eb3e7bcbd89a6d68f64d1998f936f188c929991
nirutadhital <dhitalniruta99@gmail.com>
2025-01-15 22:31:26 +0545

    feat: add dynamic routes

--- pages/niruta/index.js
+++ pages/niruta/index.js
@@ -1,6 +1,7 @@
 import styles from '../../styles/Home.module.css'
 import nirutastyles from '../../styles/niruta.module.css'
 import Head from 'next/head';
+import Link from 'next/link';
 
 export const getStaticProps = async() => {
     const res=await fetch('https://jsonplaceholder.typicode.com/users');
@@ -23,11 +24,11 @@ const Contact =({users})=>{
                 <h1>Contact Page</h1>
                 <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
                 {users.map(user => (   
-                    <div key={user.id}>
+                    <Link href={'/niruta/'+ user.id} key={user.id} legacyBehavior>
                         <a className={nirutastyles.single}>
                             <h3>{user.name}</h3>
                         </a>
-                    </div>   
+                    </Link>   
                 ))}
             </div>
         </>
